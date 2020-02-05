dca3a937797c3e44fee5460d185d6390c8c93fcb
Moe <Moe520@users.noreply.github.com>
2020-02-05 01:24:04 -0800

    finished subsetting map

--- dissemination_area_plots/subsetGeoJson.sh
+++ dissemination_area_plots/subsetGeoJson.sh
@@ -1,3 +1,4 @@
 head -n1 lda_000a16a_e.json > bc_subset_map.json
 grep 'Colombie-Britannique' lda_000a16a_e.json >> bc_subset_map.json
+sed '$ s/,$//g' bc_subset_map.json
 tail -n1 lda_000a16a_e.json   >> bc_subset_map.json
