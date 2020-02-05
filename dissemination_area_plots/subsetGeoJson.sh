1f3cdac11e8c6220296cd0bcfa6e282d9ecd5206
Moe <Moe520@users.noreply.github.com>
2020-02-05 01:26:10 -0800

    Comments for subsetting script

--- dissemination_area_plots/subsetGeoJson.sh
+++ dissemination_area_plots/subsetGeoJson.sh
@@ -1,4 +1,4 @@
-head -n1 lda_000a16a_e.json > bc_subset_map.json
-grep 'Colombie-Britannique' lda_000a16a_e.json >> bc_subset_map.json
-sed '$ s/,$//g' bc_subset_map.json
-tail -n1 lda_000a16a_e.json   >> bc_subset_map.json
+head -n1 lda_000a16a_e.json > bc_subset_map.json #copy the first line to the new file
+grep 'Colombie-Britannique' lda_000a16a_e.json >> bc_subset_map.json # Copy all features that have BC in them
+sed '$ s/,$//g' bc_subset_map.json # remove comma from last line
+tail -n1 lda_000a16a_e.json   >> bc_subset_map.json # copy last line to new file
