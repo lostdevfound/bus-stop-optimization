73bd5b0ec67582435f85a5841a3434049d068fa2
Moe <Moe520@users.noreply.github.com>
2020-02-05 21:42:40 -0800

    DA plots chloropleth map

--- dissemination_area_plots/subsetGeoJson.sh
+++
@@ -1,4 +0,0 @@
-head -n1 lda_000a16a_e.json > bc_subset_map.json #copy the first line to the new file
-grep 'Colombie-Britannique' lda_000a16a_e.json >> bc_subset_map.json # Copy all features that have BC in them
-sed -i '$s/,$//' bc_subset_map.json # remove comma from last line
-tail -n1 lda_000a16a_e.json   >> bc_subset_map.json # copy last line to new file
